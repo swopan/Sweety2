06905b6a546204b1328a52c67c23fc3f1b84441b
Sumitpatel08 <119158472+Sumitpatel08@users.noreply.github.com>
2024-06-12 11:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ from logging.handlers import RotatingFileHandler
 
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6610920261:AAFBr4g_WYrZIUAI1xOEzz7PiQTM5Dc33yw")
+APP_ID = int(os.environ.get("APP_ID", "23873858"))
+API_HASH = os.environ.get("API_HASH", "bfc7ff263795b22bb7065abe95bed3a2")
 
 
-OWNER = os.environ.get("OWNER", "") #Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", "")) #Owner user id
+OWNER = os.environ.get("OWNER", "@Patel_080") #Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", "5284816025")) #Owner user id
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002147039566"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 
