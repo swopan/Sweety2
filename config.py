d4b4265479de1f7439495b258f89d08de238bb2d
Sumitpatel08 <119158472+Sumitpatel08@users.noreply.github.com>
2024-06-24 08:19:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002147039566"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002137818418"))
 
 
 SECONDS = int(os.getenv("SECONDS", "600")) # auto delete in seconds
